59820e1401c4b685b50529f10cfe24c8d0e2d6bf
Jitao David Zhang <jitao_david.zhang@roche.com>
2014-05-19 22:26:39 +0200

    (1) malloc.h is replaced by stdlib.h (2) fixed numbers are replaced by variables

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 #define myput(x) printf(#x "=%d\n", x)
 
@@ -12,32 +12,34 @@ int* allocate(int N) {
 
 int main(int argc, char** argv) {
   int i=0;
+  int Nstat=5;
+  int Ndym=6;
 
-  int ia[5]; // a static array 
+  int ia[Nstat]; // a static array 
   int *ip;
-  for(i=0;i<5;i++) {
+  for(i=0;i<Nstat;i++) {
     ia[i]=i+1;
   }
-  for(i=0;i<5;i++) {
+  for(i=0;i<Nstat;i++) {
     myput(ia[i]);
   }
   ip=ia;
-  for(i=0;i<5;i++) {
+  for(i=0;i<Nstat;i++) {
     myput(*ip++);
   }
 
-  int* myip=allocate(6);
-  for(i=0;i<6;++i) {
+  int* myip=allocate(Ndym);
+  for(i=0;i<Ndym;++i) {
     myip[i]=i*2+1;
   }
  
   /* following commands will cause memory error since myip has changed */
-  //for(i=0;i<6;++i)
+  //for(i=0;i<Ndym;++i)
   //  myput(*myip++);
   //free(myip);
 
   /* in contrast the following lines are fine since myip has not changed*/
-  for(i=0;i<6;++i)
+  for(i=0;i<Ndym;++i)
     myput(myip[i]);
   free(myip);
   return 0;

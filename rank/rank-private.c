229fd1e5e5cec2cc5b7e378246cc0a86cbbc4361
zhangj83 <jitao_david.zhang@roche.com>
2014-05-23 15:53:58 +0200

    helper functions and functions for study purposes are moved to rank-private.c

---
+++ rank/rank-private.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include "rank-private.h"
+
+// from the following two functions, it seems clear that Item can be seen as a synonym of ItemStruct*
+Item createItem(double value, int index) {
+  Item it=(Item)malloc(sizeof(ItemStruct)); // TAKE CARE: use sizeof(Item) here will produce bugs that are very difficult to debug
+  it->index=index;
+  it->value=value;
+  it->order=0;
+  it->rank=-1.0;
+  return(it);
+}
+
+ItemStruct createItemStruct(double value, int index) {
+  ItemStruct *it=(ItemStruct*)malloc(sizeof(ItemStruct));
+  it->index=index;
+  it->value=value;
+  it->order=0;
+  it->rank=-1.0;
+  return(*it);
+}
+
+void destroyItem(Item it) {
+  free(it);
+}
+
+void destroyItemStruct(ItemStruct *it) {
+  free(it);
+}
+
+// note how to write functions for Struct and StructPtr!
+// compare this function with compareItem(const void* a, const void* b)
+int compareItemStruct (const void* a, const void* b)
+{
+  // equivalent
+  // return ((*(ItemStruct*)a).value-(*(ItemStruct*)b).value);
+  return (((ItemStruct*)a)->value-((ItemStruct*)b)->value);
+}
+
+
+// apparently rankItem can be faster than rankItemStruct because it moves only pointers, but not the whole data structures
+void rankItemStruct(ItemStruct* items, int len) {
+  int i=0;
+  qsort(items, len, sizeof(ItemStruct), compareItemStruct);
+  for(i=0;i<len;++i)
+    (items[i].index)++;
+  
+}
+
+void rankItem(Item* items, int len) {
+  int i=0;
+  qsort(items, len, sizeof(Item), compareItem);
+  for(i=0;i<len;++i)
+    (items[i]->index)++;
+}
+
+extern int Ndim; 
+extern double* values; 
+
+void useItemStruct() {
+  int i;
+  puts("---------- Using ItemStruct ----------");
+  ItemStruct *ilist=(ItemStruct*)malloc(Ndim*sizeof(ItemStruct));
+
+  puts("Before sorting");
+  for(i=0;i<Ndim;++i) {
+    ilist[i]=createItemStruct(values[i], i);
+    printf("ilist[%d]=%.2f, index=%d, rank=%.1f\n", 
+	   i, ilist[i].value, ilist[i].index, ilist[i].rank);
+  }
+  rankItemStruct(ilist, Ndim);
+  puts("After sorting");
+  for(i=0;i<Ndim;++i) {
+    printf("ilist[%d]=%.2f, order=%d, rank=%.1f\n", 
+	   i, ilist[i].value, ilist[i].index, ilist[i].rank);
+  }
+}
---
+++ rank/rank-private.h
@@ -0,0 +1,9 @@
+#ifndef RANK_PRIVATE_H_
+#define RANK_PRIVATE_H_
+
+#include "rank.h"
+
+Item createItem(double, int);
+void destroyItem(Item it);
+
+#endif /* RANK_PRIVATE_H */

800f981a04afbc53d380fe6c3e53ef6722dc3047
zhangj83 <jitao_david.zhang@roche.com>
2014-05-25 15:52:07 +0200

    refactor the simple rank function out of wmw_test into wmw_test_private

--- wmw_test/wmw_test.c
+++ wmw_test/wmw_test.c
@@ -89,23 +89,6 @@ void wmwResDestroy(wmwRes res) {
   free(res);
 }
 
-/*! \brief rank an array of double values and fill in ranks
- * the function comes from http://nathsaharia.blogspot.ch/2012/09/implement-rank-sort-in-c.html 
- * Unfortunately it can be slow when the input array is large.
-*/
-void rank(const dArray values, iArray ranks) {
-  double* v=values->value;
-  int* r=ranks->value;
-  int len=values->len;
-  int i,j;
-  for(i=0;i<len;++i) r[i]=1; // rank starts from 1
-  for(i=0;i<len;++i) {
-    for(j=0;j<i;++j) { /* O(n2) */
-      v[j]<=v[i] ? r[i]++ : r[j]++;
-    }
-  }
-}
-
 wmwRes wmwTest(const iArray index,
 	       const dArray stat,
 	       const double cor,
